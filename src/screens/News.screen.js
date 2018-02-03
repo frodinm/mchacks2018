e73eb055df891389b22277e901cb7426c42d3874
frodinm <fabrizio_1197@hotmail.com>
2018-02-03 18:20:40 -0500

    fix

--- src/screens/News.screen.js
+++ src/screens/News.screen.js
@@ -1,19 +1,42 @@
 import React, { Component } from 'react';
 import {NavigationBar} from '../components';
+import { getTopNewsHeadlines } from '../api'
+import { Card } from 'antd';
 
 export class NewsScreen extends Component {
+    constructor(){
+        super();
+        this.state={
+            topNewsStories: {},
+            error: null
+        }
+    }
     componentDidMount(){
         getTopNewsHeadlines().then((res)=>{
-            console.log(res)
+            this.setState({
+                topNewsStories: res.data.articles
+            })
+            console.log(this.state.topNewsStories)
         }).catch((err)=>{
-            console.log(err)
+            this.setState({
+                error: err
+            })
         })
     }
+
+    handleNewsCard(){
+
+    }
+
   render() {
     return (
       <div className="main">
         <NavigationBar />
-        
+        <Card title="Card title" extra={<a href="#">More</a>} style={{ width: 300 }}>
+            <p>Card content</p>
+            <p>Card content</p>
+            <p>Card content</p>
+        </Card>
       </div>
     );
   }
