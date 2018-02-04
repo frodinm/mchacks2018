dfef903b83017fdf8a9391cbeb510cafb456b2b2
frodinm <fabrizio_1197@hotmail.com>
2018-02-04 08:05:16 -0500

    update

--- src/components/chatbot.component.js
+++ src/components/chatbot.component.js
@@ -1,6 +1,15 @@
 import React, { Component } from 'react';
+import triangle from '../assets/triangle.png'
+import {TweenLite} from 'gsap'
 
 export class ChatBotComponent extends Component {
+  constructor(){
+    super();
+    this.state={
+      chatIsShowing: false
+    }
+    this.handleAnimation = this.handleAnimation.bind(this);
+  }
 
 
   componentDidMount(){
@@ -10,12 +19,41 @@ export class ChatBotComponent extends Component {
         spaceId: 'Y2lzY29zcGFyazovL3VzL1JPT00vN2NkZTQ3M2UtNmFkZi0zODkxLWFhODYtNzdkMDUyYTM3ZGZm'
       });
   }
+ 
+  handleAnimation(){
+    if(this.state.chatIsShowing === false){
+      this.setState({
+        chatIsShowing: true
+      })
+      TweenLite.to(document.getElementById('myWidget'),1,{opacity:1})
+      TweenLite.to(document.getElementById('triangle'),1,{opacity:1})
+    }else{
+      this.setState({
+        chatIsShowing: false
+      })
+      TweenLite.to(document.getElementById('myWidget'),1,{opacity:0})
+      TweenLite.to(document.getElementById('triangle'),1,{opacity:0})
+    }
+  }
 
 
   render() {
+    const content = (
+      <div>
+        
+      </div>
+    );
+    
     return (
-        <div>  
-            <div id="myWidget" style={{height:500,width:400}}/>
+        <div >  
+            <div  onClick={()=>this.handleAnimation()} className="fab-button">
+              <img src='https://c74213ddaf67eb02dabb-04de5163e3f90393a9f7bb6f7f0967f1.ssl.cf1.rackcdn.com/V1~23492bc83a3b3af41cb55d4b8923b534~SJ6QIupfTSm6Hh1CsuKwFQ==~110'/>
+            </div>
+            <div id="myWidget" style={{height:400,width:350}}/>
+            <svg id="triangle" viewBox = "0 0 500 500">   
+              <polygon points="195,140 245,210 140,210" style={{fill:'white'}}></polygon>
+
+          </svg>
         </div>
     );
   }
